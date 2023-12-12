8070bcca04c533583cb0f48a28ade6b862b461ed
Bauman Student <bauman@example.com>
2023-12-12 20:20:34 +0300

    Загрузка проекта

--- bank/grpc_client.py
+++ bank/grpc_client.py
@@ -4,7 +4,7 @@ import bank.my_proto_pb2_grpc
 class MyStub:
     def __init__(self, server_address):
         channel = grpc.insecure_channel(server_address)
-        self.stub = bankZHAB.my_proto_pb2_grpc.MyServiceStub(channel)
+        self.stub = bank.my_proto_pb2_grpc.MyServiceStub(channel)
 
     def my_method(self, request):
         return self.stub.MyMethod(request)
